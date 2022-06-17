e52055748e20748f22f08bcab9b34b6fc4556da7
Arfat Salman <giney.paradise@gmail.com>
2022-06-17 18:13:54 +0200

    capitalize the name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Arfat Salman
+          Learn React by ARFAT SALMAN
         </a>
       </header>
     </div>
