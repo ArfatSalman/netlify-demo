fc310281f296b63c3b8af383d691f33e92e0852c
Arfat Salman <giney.paradise@gmail.com>
2022-06-17 18:07:30 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Arfat Salman
         </a>
       </header>
     </div>
